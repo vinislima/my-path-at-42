f103ab3f3519ce5f549bd92b35689c0f1eb6e0ec
Vinícius Lima <vinislima@gmail.com>
2024-10-11 11:59:48 -0300

    inclusão de comentários

--- piscine/c01/c01/ex04/ft_ultimate_div_mod.c
+++ piscine/c01/c01/ex04/ft_ultimate_div_mod.c
@@ -11,14 +11,43 @@
 /* ************************************************************************** */
 
 void	ft_ultimate_div_mod(int *a, int *b);
-
+// aqui o objetivo é manipular a informação no ponteiro
+// de tal forma que com a função, os ponteiros recebam
+// os valores e com neles mesmo façamos a alteração de memória
+// armazenando os resultados
 void	ft_ultimate_div_mod(int *a, int *b)
 {
+	// declaramos duas variáveis para momentaneamente
+	// armazene os valores dos cálculos
 	int	div;
 	int	mod;
-
+	// div recebe o valor da divisão dos valores entre a e b
 	div = *a / *b;
+	// mod recebe o valor do módulo dos valores entre a e b
 	mod = *a % *b;
+	// aqui ponteiro recebe o valor de div
 	*a = div;
+	// aqui o ponteiro recebe o valor de mod
 	*b = mod;
+}
+
+// início da main
+#include <unistd.h>
+#include <stdio.h>
+
+void	ft_ultimate_div_mod(int *a, int *b);
+
+int	main()
+{
+	// declaramos e atribuímos valores as variáveis
+	int a = 40;
+	int b = 10;
+	// imprimimos para verificar os valores armazenados
+	printf("a = %i, b = %i\n", a, b);
+	// chamamos a função e passamos os endereços de memórias das variáveis
+	ft_ultimate_div_mod(&a, &b);
+	// imprimimos novamente os valores das variáveis, que agora contém
+	// os valores dos cálculos realizados na função
+	printf("a = %i, b = %i", a, b);
+	return(0);
 }
--- piscine/c01/c01/ex05/ft_putstr.c
+++ piscine/c01/c01/ex05/ft_putstr.c
@@ -11,16 +11,26 @@
 /* ************************************************************************** */
 
 #include <unistd.h>
-
+// aqui vamos imprimir strings
+// o ponteiro recebido aponta para o início da string
 void	ft_putstr(char *str);
 
 void	ft_putstr(char *str)
 {
+	// declaramos uma variável que servirá de contador
+	// no laço de repetição
 	int	i;
-
+	// atribuímos o valor inicial a variável
 	i = 0;
+	// esse laço se repetirá enquanto o elemento
+	// da string for diferente de nulo
+	// o contador irá proporcionar que seja possível
+	// percorrer a string
 	while (str[i] != '\0')
 	{
+		// utilizando a função write, será impresso
+		// cada elemento da string, até que seja atingido
+		// o elemento nulo
 		write(1, &str[i], 1);
 		i++;
 	}
