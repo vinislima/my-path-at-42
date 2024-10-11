2ff7b9783f456649dc025ec08e2e6ba95af53bed
Vinícius Lima <vinislima@gmail.com>
2024-10-11 17:56:31 -0300

    comentários

--- piscine/c02/c02/ex01/ft_strncpy.c
+++ piscine/c02/c02/ex01/ft_strncpy.c
@@ -10,6 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// parecida com a strcpy, essa função faz a cópia de strins, mas até um limite informado
 char	*ft_strncpy(char *dest, char *src, unsigned int n);
 
 char	*ft_strncpy(char *dest, char *src, unsigned int n)
@@ -19,15 +20,43 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 
 	limit = n;
 	count_size = 0;
+	// nesse laço enquanto o contador for menor que o limite informado
+	// e elemento do array original for diferente de nulo e ocorrerá
 	while (count_size < limit && src[count_size] != '\0')
 	{
+		// aqui ocorre a cópia, com o elemento do array recebendo o
+		// seu correspondente do array de origem
 		dest[count_size] = src[count_size];
 		count_size++;
 	}
+	// esse segundo elemento ocorre para em caso do array de destino
+	// ser maior que o origem, os elementos restantes serão preenchidos
+	// com nulos
 	while (count_size < limit)
 	{
 		dest[count_size] = '\0';
 		count_size++;
 	}
+	// ocorre o retorno do array de destino
 	return (dest);
+}
+
+// início da string
+#include <stdio.h>
+
+char	*ft_strncpy(char *dest, char *src, unsigned int n);
+
+int	main(void)
+{
+	// declaramos uma string, que será a origem
+	char *src = "Teste";
+	// declaramos uma string que será usada como destino
+	// e colocamos um tamanho nela
+	char dest[5];
+	// aqui declaramos e atribuimos um valor de limite
+	unsigned int n = 3;
+	// chamamos a funação diretamente dentro do printf para
+	// impressão da saída dela
+	printf("%s",ft_strncpy(dest, src, n));
+	return(0);
 }
--- piscine/c02/c02/ex02/ft_str_is_alpha.c
+++ piscine/c02/c02/ex02/ft_str_is_alpha.c
@@ -10,9 +10,14 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+// nesta função iremos verificar se todos os elementos
+// de um array são letras
 int	ft_str_is_alpha(char *str);
 int	ft_strlen(char *str);
 
+// criamos uma função para saber o tamanho da array
+// pois será utilizado em um dos testes de validação
+// da função principal
 int	ft_strlen(char *str)
 {
 	int	i;
@@ -28,20 +33,33 @@ int	ft_strlen(char *str)
 int	ft_str_is_alpha(char *str)
 {
 	int	count_str;
-
+	// nesse teste, caso a a string não tenha nenhum elemento
+	// retornamos o valor um, conforme solicitado
 	if (ft_strlen(str) <= 0)
 		return (1);
 	else
 	{
+		// assim como em exercícios anteriores, percorreremos os
+		// elementos do array enquanto ele for diferente de nulo
 		count_str = 0;
 		while (str[count_str] != '\0')
 		{
+			// nesse teste verificamos o elemento junto aos valores
+			// da tabela ascii, caso esteja fora do intervalo de letras
+			// retornaremos o valor zero, que significa que dentro do array
+			// há um elemento difente de letra
 			if (str[count_str] <= 64 || str[count_str] >= 123)
 				return (0);
+			// também verificamos o intervalo correspondente da tabela
+			// ascii entre letras maiscúlas e minúsculas, nesse caso se
+			// o elemento está dentro do intervalo, em caso positivo
+			// retornamos o valor de zero também
 			else if (str[count_str] >= 91 && str[count_str] <= 96)
 				return (0);
 			count_str++;
 		}
 	}
+	// caso tenha passado por toda a string sem entrar em nenhuma das condicionais
+	// significa que na string há somente letras
 	return (1);
 }
